e5bc5c50c665fa534f15e990b2c1cd1076b2c87a
anhyeongkwan <bombom841@naver.com>
2024-11-18 23:44:43 +0900

    - init

---
+++ toy_project/src/main/java/com/attendance/vo/Member.java
@@ -0,0 +1,24 @@
+package com.attendance.vo;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+  private int userIdx; // 사용자 고유 번호
+  private String userId; // 사용자 ID
+  private String userPassword; // 비밀번호
+  private String userName; // 사용자 이름
+  private String userPhone; // 사용자 전화번호
+  private String userEmail; // 사용자 이메일
+  private LocalDateTime userCreateDate; // 생성일
+  private LocalDateTime userUpdateDate; // 수정일
+  private int userFailCnt; // 로그인 실패 횟수
+  private int userStatus; // 사용자 상태
+
+}
